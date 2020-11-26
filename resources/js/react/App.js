c1c8cffb736b8c43806233a5dce950f316ae959d
GeorgeHope <georgehopekek@gmail.com>
2020-11-26 23:01:39 +0100

    Dynamical change the page title

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import Header from "./components/main/Header";
+import Sidebar from "./components/main/Sidebar";
+import PageContainer from "./components/main/PageContainer";
 
 function App() {
+    const [title, setTitle] = React.useState(false);
+
+    React.useEffect(() => getTitle(), []);
+
+    const getTitle = () => {
+        let location = window.location.pathname;
+
+        const titles = [
+            {
+                path: '/',
+                title: 'Dashboard',
+            }
+        ];
+
+        titles.forEach((element) => {
+            if (element.path === location) {
+                return setTitle(element.title);
+            }
+        });
+    }
+
     return (
         <Router>
             <Header/>
-            <Switch>
-                <Route path={'/'} component={HomePage}/>
-            </Switch>
+            <div className='main-content columns main-content-custom'>
+                <Sidebar/>
+                <Switch>
+                    <PageContainer title={title || ''}>
+                        <Route path={'/'} component={HomePage}/>
+                    </PageContainer>
+                </Switch>
+            </div>
         </Router>
     );
 }
