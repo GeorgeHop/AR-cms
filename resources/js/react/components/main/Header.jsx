246e7579f40817aae7ece3d327af8a25f0553e66
GeorgeHope <georgehopekek@gmail.com>
2020-11-27 14:16:35 +0100

    Remove buttons

--- resources/js/react/components/main/Header.jsx
+++ resources/js/react/components/main/Header.jsx
@@ -9,16 +9,10 @@ const Header = () => {
             </div>
 
             <div className='navbar-menu'>
-                <div className='navbar-start'>
-                    <NavLink className='navbar-item' to={'/'}>Home</NavLink>
-                    <NavLink className='navbar-item' to={'/'}>Home</NavLink>
-                </div>
-
                 <div className='navbar-end'>
                     <div className='navbar-item'>
                         <div className='buttons'>
-                            <button className='button is-primary'>Test</button>
-                            <button className='button is-light'>Test</button>
+                            <button className='button is-light'>Log out</button>
                         </div>
                     </div>
                 </div>
