b57ee712dfc8362a14354028c400c5e20ecb1bad
Dmytro Kardava <dima.kardava@gmail.com>
2020-12-01 21:12:36 +0100

    move redux provider

--- resources/js/App.js
+++ resources/js/App.js
@@ -2,28 +2,29 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const RouteTitles = {
     '/': 'Dashboard',
 };
 
 function App() {
-    const location = window.location.pathname;
-
     return (
-        <Router>
-            <Switch>
-
-                {/*Routes for main Layout*/}
-                <Route>
-                    <MainLayout appName={'AR-cms'} title={RouteTitles[location] || ''}>
-                        <Switch>
-                            <Route exact path={'/'} component={HomePage}/>
-                        </Switch>
-                    </MainLayout>
-                </Route>
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    {/*Routes for main Layout*/}
+                    <Route>
+                        <MainLayout appName={'AR-cms'} title={'' /*todo get title inside main layout*/}>
+                            <Switch>
+                                <Route exact path={} component={HomePage}/>
+                            </Switch>
+                        </MainLayout>
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
     );
 }
 
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
-import {Provider} from "react-redux";
-import store from "./redux/store";
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('app')
-);
-
+ReactDOM.render(<App/>, document.getElementById('app'));
