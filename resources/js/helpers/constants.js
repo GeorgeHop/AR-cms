e0807e7f7745413b0c93776618b6b34c2a275c2f
Dmytro Kardava <dima.kardava@gmail.com>
2020-12-01 21:18:32 +0100

    introduce route constants

--- resources/js/helpers/constants.js
+++ resources/js/helpers/constants.js
@@ -1,1 +1,12 @@
 export const DefaultUserImage = '/images/user.png';
+
+export const SiteName = 'AR-CMS';
+
+export const Routes = {
+    Home: '/',
+    Login: '/login',
+};
+
+export const RouteTitles = {
+    [Routes.Home]: 'Home',
+};
--- resources/js/layouts/MainLayout.jsx
+++ resources/js/layouts/MainLayout.jsx
@@ -3,23 +3,22 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import * as classnames from "classnames";
 import {connect} from "react-redux";
+import {RouteTitles, SiteName} from "../helpers/constants";
 
-const MainLayout = ({isOpen, children, title, appName}) => {
-    return (
-        <>
-            <div className={'d-flex'}>
-                <Sidebar/>
-                <div id={'content'} className={classnames('bg-light', {open: isOpen})}>
-                    <Header name={appName}/>
-                    <div className="p-3 content-inner-shadow">
-                        <h4 className='m-1'>{title}</h4>
-                        {children}
-                    </div>
+const MainLayout = ({isOpen, children}) => (
+    <>
+        <div className={'d-flex'}>
+            <Sidebar/>
+            <div id={'content'} className={classnames('bg-light', {open: isOpen})}>
+                <Header name={SiteName}/>
+                <div className="p-3 content-inner-shadow">
+                    <h4 className='m-1'>{RouteTitles[window.location.pathname] || ''}</h4>
+                    {children}
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    </>
+);
 
 export default connect(state => ({
     isOpen: state.sidebarReducer,
