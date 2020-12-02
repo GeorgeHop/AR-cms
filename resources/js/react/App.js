875e9747214ff48f96418648739c72e6421c36f6
GeorgeHope <georgehopekek@gmail.com>
2020-12-02 16:08:49 +0100

    Refactor App.js

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -2,29 +2,30 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import Login from "./pages/Login";
+import AdminsList from "./pages/Admins/AdminsList";
+import AdminDetails from "./pages/Admins/AdminDetails";
+import Routes from "./routes/Routes";
 
-const RouteTitles = {
-    '/': 'Dashboard',
-};
-
-function App() {
-    const location = window.location.pathname;
-
-    return (
-        <Router>
-            <Switch>
-
-                {/*Routes for main Layout*/}
-                <Route>
-                    <MainLayout appName={'AR-cms'} title={RouteTitles[location] || ''}>
-                        <Switch>
-                            <Route exact path={'/'} component={HomePage}/>
-                        </Switch>
-                    </MainLayout>
-                </Route>
-            </Switch>
-        </Router>
-    );
-}
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path={'/login'} component={Login}/>
+            {/*Routes for main Layout*/}
+            <Route>
+                <MainLayout appName={'AR-cms'}>
+                    <Switch>
+                        {/* Dashboard */}
+                        <Route exact path={Routes.Dashboard} component={HomePage}/>
+                        {/* Admin pages */}
+                        <Route exact path={Routes.AdminsList} component={AdminsList}/>
+                        <Route exact path={Routes.AdminEdit()} component={AdminDetails}/>
+                        <Route exact path={Routes.AdminCreate} component={AdminDetails}/>
+                    </Switch>
+                </MainLayout>
+            </Route>
+        </Switch>
+    </Router>
+)
 
 export default App;
