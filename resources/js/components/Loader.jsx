16ba71eb56a1730f6337b8b881efa9d5ba6a8f4b
GeorgeHope <georgehopekek@gmail.com>
2020-12-09 20:26:44 +0100

    Added loader component

---
+++ resources/js/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = ({isLoading}) => (
+    isLoading && (
+        <div className="h-100 d-flex justify-content-center" style={{alignItems: 'center'}}>
+            <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    )
+);
+
+export default Loader;
