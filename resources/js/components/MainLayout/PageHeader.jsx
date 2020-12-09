77c67c3a79cde832261216cc521c6f54350a1476
GeorgeHope <georgehopekek@gmail.com>
2020-12-09 16:45:16 +0100

    Added notification for page header

--- resources/js/components/MainLayout/PageHeader.jsx
+++ resources/js/components/MainLayout/PageHeader.jsx
@@ -3,15 +3,23 @@ import Row from "react-bootstrap/cjs/Row";
 import Button from "react-bootstrap/cjs/Button";
 import {useHistory} from 'react-router-dom';
 
-const PageHeader = ({buttonLabel, title, buttonRoute}) => {
+const PageHeader = ({buttonLabel, title, buttonRoute, notification}) => {
     const history = useHistory();
 
     return (
-        <Row className='m-1 justify-content-between'>
-            <h4 className='m-1'>{title}</h4>
-            {!!buttonLabel &&
-            <Button className='btn-sm' onClick={() => history.push(buttonRoute)}>{buttonLabel}</Button>}
-        </Row>
+        <>
+            {!!notification && (
+                <div id='header-notification' className="alert alert-primary" role="alert">
+                    {notification}
+                </div>
+            )}
+            <Row className='m-1 justify-content-between'>
+                <h4 className='m-1'>{title}</h4>
+                {!!buttonLabel &&
+                <Button className='btn-sm' onClick={() => history.push(buttonRoute)}>{buttonLabel}</Button>
+                }
+            </Row>
+        </>
     )
 }
 
