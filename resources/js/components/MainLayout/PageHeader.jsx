23d0f035136c4b650425fd4a0dea816c6fcc01a9
GeorgeHope <georgehopekek@gmail.com>
2020-12-10 15:56:34 +0100

    Added search bar

--- resources/js/components/MainLayout/PageHeader.jsx
+++ resources/js/components/MainLayout/PageHeader.jsx
@@ -3,7 +3,7 @@ import Row from "react-bootstrap/cjs/Row";
 import Button from "react-bootstrap/cjs/Button";
 import {useHistory} from 'react-router-dom';
 
-const PageHeader = ({buttonLabel, title, buttonRoute, notification}) => {
+const PageHeader = ({buttonLabel, title, buttonRoute, notification, searchOnChange}) => {
     const history = useHistory();
 
     return (
@@ -13,10 +13,22 @@ const PageHeader = ({buttonLabel, title, buttonRoute, notification}) => {
                     {notification}
                 </div>
             )}
-            <Row className='m-1 justify-content-between'>
+            <Row className='m-2 justify-content-between'>
                 <h4 className='m-1'>{title}</h4>
+                {!!searchOnChange && (
+                    <div className="d-flex">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                            onChange={searchOnChange}
+                        />
+                        <button className="btn btn-outline-success ml-1" type="submit">Search</button>
+                    </div>
+                )}
                 {!!buttonLabel &&
-                    <Button className='btn-sm' onClick={() => history.push(buttonRoute)}>{buttonLabel}</Button>
+                <Button className='btn-sm' onClick={() => history.push(buttonRoute)}>{buttonLabel}</Button>
                 }
             </Row>
         </>
